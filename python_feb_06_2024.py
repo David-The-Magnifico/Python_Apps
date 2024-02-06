1db2db24d91bdc51fae6e25568c1039a0ecb2f97
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-02-06 15:40:49 +0100

    Update python_feb_06_2024.py

--- python_feb_06_2024.py
+++ python_feb_06_2024.py
@@ -40,3 +40,11 @@ if name.isalpha():
         print("valid name")
 else:
         print("invalid name")
+
+
+
+name = input("enter your name: ").strip()
+if name.isdigit():
+        print("valid name")
+else:
+        print("invalid name")
