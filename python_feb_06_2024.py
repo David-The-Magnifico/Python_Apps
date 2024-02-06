d09c294a3bb070f9d25b164b7c234034c769f072
David <www.wealthydavid@gmail.com>
2024-02-06 14:26:07 +0100

    String Manipulation(commit on FEB'2024)

---
+++ python_feb_06_2024.py
@@ -0,0 +1,14 @@
+name1 = "bolaji"
+name3 = "chichi"
+name2 = "dayo"
+
+
+print(name1 == name2)
+print(name1 == name2)
+print(ord('B'))
+print("C" in  name3)
+
+
+for char in name3:
+    if char == "C":
+        
