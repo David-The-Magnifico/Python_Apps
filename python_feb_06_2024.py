53caa985c7e53937d19d67f849f2c6329e0aedea
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-02-06 14:36:25 +0100

    Update python_feb_06_2024.py

--- python_feb_06_2024.py
+++ python_feb_06_2024.py
@@ -9,6 +9,8 @@ print(ord('B'))
 print("C" in  name3)
 
 
-for char in name3:
-    if char == "C":
+print(f'[{name1:10}]')
+print(f'[{name1:>10}]')
+print(f'[{name1:<10}]')
+print(f'[{name1:^10}]')
         
